0f8e5fdc30fe1c44d5c154813e68c7c2c89c5a64
Derek Schwantner <derek@Dereks-Mac-mini.local>
2021-11-27 21:41:19 -0500

    deploy.py now fetches the bytecode and abi from the json file

---
+++ contracts/deploy.py
@@ -0,0 +1,33 @@
+from solcx import compile_standard, install_solc
+import json
+
+with open("./SimpleStorage.sol", "r") as file:
+    simple_storage_file = file.read()
+    print(simple_storage_file)
+
+# Compile Our Solidity Contract
+install_solc("0.6.0")
+
+compiled_sol = compile_standard(
+    {
+        "language": "Solidity",
+        "sources": {"SimpleStorage.sol": {"content": simple_storage_file}},
+        "settings": {
+            "outputSelection": {
+                "*": {"*": ["abi", "metadata", "evm.bytecode", "evm.sourceMap"]}
+            }
+        },
+    },
+    solc_version="0.6.0",
+)
+
+with open("compiled_code.json", "w") as file:
+    json.dump(compiled_sol, file)
+
+# get bytecode, this is walking down the json tree to access the bytecode
+bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"][
+    "bytecode"
+]["object"]
+
+# get abi
+abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
