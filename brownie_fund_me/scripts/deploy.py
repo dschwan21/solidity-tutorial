eb5bb41dd997e19b466f772e913e6a725d37e469
Derek Schwantner <derek@Dereks-Mac-mini.local>
2021-12-08 23:46:45 -0500

    importing from helpful scripts instead of hard coding the account function

--- brownie_fund_me/scripts/deploy.py
+++ brownie_fund_me/scripts/deploy.py
@@ -1,8 +1,11 @@
 from brownie import FundMe
+from scripts.helpful_scripts import get_account
 
 
 def deploy_fund_me():
     account = get_account()
+    fund_me = FundMe.deploy({"from": account})
+    print(f"FundMe deployed at {fund_me.address}")
 
 
 def main():
